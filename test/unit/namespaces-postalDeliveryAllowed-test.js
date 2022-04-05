9ded085469267d6932767e3f9e9458a440fa7a42
Simon Varney <varney@users.noreply.github.com>
2022-04-05 06:57:12 +0200

    Rename and move namespaces helper

---
+++ test/unit/namespaces-postalDeliveryAllowed-test.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const {postalDeliveryAllowed} = require("../../lib/lambdas/namespaces");
+
+const scenarios = [
+  {
+    expected: true,
+    namespace: "dn",
+    text: "DN"
+  },
+  {
+    expected: false,
+    namespace: "some-namespace",
+    text: "Some namespace"
+  }
+];
+
+describe("check if postal delivery allowed", () => {
+  for (const s of scenarios) {
+    describe(s.text, () => {
+      const postOk = postalDeliveryAllowed(s.namespace);
+
+      it(`should${!s.expected ? " not" : ""} allow postal delivery`, () => {
+        Boolean(postOk).should.eql(Boolean(s.expected));
+      });
+    });
+  }
+});
