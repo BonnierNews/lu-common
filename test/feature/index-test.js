0bd638bccbf22aa7e4cf3fa4c621acbed04e95a4
Konrad Guzior <36774785+MrGuzior@users.noreply.github.com>
2022-12-01 16:21:13 +0100

    lu-common-package (#32)
    * lu-common-package
    
    * Update github actions
    
    * Update config
    
    * Fix typo
    
    * Use node 16
    
    * Export test helpers
    
    * Add parameter to fakeApi
    
    * Revert is
    
    * Adjust order
    
    * Add stripSchemaTag
    
    * Export country sodes as array
    
    * Add test
    
    * What
    
    * Remove .only
    
    * Lower coverage threshold
    
    * Update coverage
    
    * Add sinon as peerDep
    
    * Remove after-hook
    
    * Change order for debugging
    
    * Make nock dep
    
    * Bump sinon
    
    * Remove unnecessary change

---
+++ test/feature/index-test.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const common = require("../../index");
+const expect = require("chai").expect;
+
+const expectedExports = [
+  "namespaces",
+  "schemas",
+  "email",
+  "ftp",
+  "gcs",
+  "iterators",
+  "PDF",
+  "pdfGenerator",
+  "s3",
+  "ses",
+  "sftp",
+  "streams",
+  "fakeApi",
+  "fakeFtp",
+  "fakeGcs",
+  "fakeS3",
+  "fakeSes",
+  "fileUtils",
+  "messageHelper",
+  "pdfReader",
+  "stripSchemaTag",
+  "countryCodes"
+];
+
+describe("Exposed features", () => {
+  const exports = [];
+
+  for (const c in common) {
+    exports.push(c.toString());
+  }
+
+  describe("Importing default export", () => {
+    it(`The right stuff gets imported`, () => {
+      const list = exports.filter((val) => !expectedExports.includes(val));
+      const list2 = expectedExports.filter((val) => !exports.includes(val));
+      expect(list.length).to.equal(0);
+      expect(list2.length).to.equal(0);
+    });
+  });
+});
