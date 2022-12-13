e65887dc2fe56bfa015988ddb56b6ab5e184a93f
BN-CRehn <97894666+BN-CRehn@users.noreply.github.com>
2022-12-13 11:46:52 +0100

    Add distributionFeeSchema

--- test/unit/validation-helpers/schemas-test.js
+++ test/unit/validation-helpers/schemas-test.js
@@ -1,225 +1,14 @@
 "use strict";
 
-const {addressSchema} = require("../../../lib/validation-helpers/schemas");
+const {addressSchema, distributionFeeSchema} = require("../../../lib/validation-helpers/schemas");
 
-const stripSchemaTag = require("../../../lib/validation-helpers/strip-joi-schema-tags");
-
-const scenarios = [
-  {
-    text: "Valid address with all the fields",
-    expected: true,
-    address: {
-      streetName: "Testgatan",
-      streetNumber: "1",
-      stairCase: "A",
-      stairs: "1",
-      apartmentNumber: "1101",
-      zipCode: "12345",
-      city: "Teststaden",
-      careOf: "Bestefar",
-      companyName: "AwesomeCompany AB",
-      country: "SE",
-      deliveryMethod: "POST"
-    }
-  },
-  {
-    text: "Valid foregin address",
-    expected: true,
-    address: {
-      streetName: "Awesome street in canada",
-      streetNumber: "1",
-      zipCode: "S0J 2Y0",
-      city: "Saskatchewan",
-      country: "CA"
-    }
-  },
-  {
-    text: "Valid address with normal fields",
-    expected: true,
-    address: {
-      streetName: "Testgatan",
-      streetNumber: "1",
-      zipCode: "12345",
-      city: "Teststaden",
-      country: "SE"
-    }
-  },
-  {
-    text: "Valid address with normal fields, normal spacing in swedish zipcode",
-    expected: true,
-    address: {
-      streetName: "Testgatan",
-      streetNumber: "1",
-      zipCode: "123 45",
-      city: "Teststaden",
-      country: "SE"
-    }
-  },
-  {
-    text: "Valid address with normal fields, missing country so default to SE",
-    expected: true,
-    address: {
-      streetName: "Testgatan",
-      streetNumber: "1",
-      zipCode: "12345",
-      city: "Teststaden"
-    }
-  },
-  {
-    text: "Valid address with normal fields, some null values",
-    expected: true,
-    address: {
-      streetName: "Testgatan",
-      streetNumber: "1",
-      stairCase: null,
-      stairs: null,
-      apartmentNumber: null,
-      zipCode: "12345",
-      city: "Teststaden",
-      careOf: "Bestefar",
-      companyName: null,
-      country: null,
-      deliveryMethod: null
-    }
-  },
-  {
-    text: "Valid address with minimum fields",
-    expected: true,
-    address: {
-      streetName: "Testgatan",
-      zipCode: "12345",
-      city: "Teststaden",
-      country: "SE"
-    }
-  },
-  {
-    text: "Invalid address default country address, bad zipCode",
-    expected: false,
-    error: '"zipCode" failed validation because Postal code A123 is not valid for country SE',
-    address: {
-      streetName: "Testgatan",
-      streetNumber: "1",
-      zipCode: "A123",
-      city: "Teststaden"
-    }
-  },
-  {
-    text: "Invalid address cause of zipCode to long",
-    expected: false,
-    error: '"zipCode" failed validation because Postal code 123456789 is not valid for country SE',
-    address: {
-      streetName: "Testgatan",
-      streetNumber: "1",
-      stairCase: "A",
-      stairs: "1",
-      apartmentNumber: "1101",
-      zipCode: "123456789",
-      city: "Teststaden",
-      careOf: "Bestefar",
-      companyName: "AwesomeCompany AB",
-      country: "SE"
-    }
-  },
-  {
-    text: "Invalid address cause of zipCode to short",
-    expected: false,
-    error: '"zipCode" failed validation because Postal code 1337 is not valid for country SE',
-    address: {
-      streetName: "Testgatan",
-      streetNumber: "1",
-      stairCase: "A",
-      stairs: "1",
-      apartmentNumber: "1101",
-      zipCode: "1337",
-      city: "Teststaden",
-      careOf: "Bestefar",
-      companyName: "AwesomeCompany AB",
-      country: "SE"
-    }
-  },
-  {
-    text: "Invalid address cause of missing streetName",
-    expected: false,
-    error: '"streetName" is required',
-    address: {
-      streetNumber: "1",
-      stairCase: "A",
-      stairs: "1",
-      apartmentNumber: "1101",
-      zipCode: "12345",
-      city: "Teststaden",
-      careOf: "Bestefar",
-      companyName: "AwesomeCompany AB",
-      country: "SE"
-    }
-  },
-  {
-    text: "Invalid address cause of missing city",
-    expected: false,
-    error: '"city" is required',
-    address: {
-      streetName: "Testgatan",
-      streetNumber: "1",
-      stairCase: "A",
-      stairs: "1",
-      apartmentNumber: "1101",
-      zipCode: "12345",
-      careOf: "Bestefar",
-      companyName: "AwesomeCompany AB",
-      country: "SE"
-    }
-  },
-  {
-    text: "Invalid address cause of invalid country",
-    expected: false,
-    error: '"country" must be one of',
-    address: {
-      streetName: "Testgatan",
-      streetNumber: "1",
-      stairCase: "A",
-      stairs: "1",
-      apartmentNumber: "1101",
-      zipCode: "12345",
-      city: "Teststaden",
-      careOf: "Bestefar",
-      companyName: "AwesomeCompany AB",
-      country: "BLAH"
-    }
-  },
-  {
-    text: "Invalid address cause of invalid foregin zipcode",
-    expected: false,
-    error: '"zipCode" failed validation because Postal code 12 is not valid for country US',
-    address: {
-      streetName: "Testgatan",
-      streetNumber: "1",
-      stairCase: "A",
-      stairs: "1",
-      apartmentNumber: "1101",
-      zipCode: "12",
-      city: "Teststaden",
-      careOf: "Bestefar",
-      companyName: "AwesomeCompany AB",
-      country: "US"
-    }
-  },
-  {
-    text: "Invalid, address is empty",
-    expected: false,
-    error: '"streetName" is required',
-    address: {}
-  },
-  {
-    text: "Valid, stripped address is empty",
-    strippedSchema: stripSchemaTag(addressSchema, "test"),
-    expected: true,
-    stripped: true,
-    address: {}
-  }
-];
+const {
+  addressScenarios,
+  distributionFeeScenarios
+} = require("../../../test-data/unit/validation-helpers/schemas-test-data");
 
 describe("check if address is correct", () => {
-  for (const s of scenarios) {
+  for (const s of addressScenarios) {
     describe(s.text, () => {
       const {value, error: notValidAddress} = s.stripped
         ? s.strippedSchema.validate(s.address, {stripUnknown: true})
@@ -238,6 +27,24 @@ describe("check if address is correct", () => {
       if (!s.expected) {
         it(`should have the error message: '${s.error}'`, () => {
           notValidAddress.details[0].message.should.contain(s.error);
+        });
+      }
+    });
+  }
+});
+
+describe("check if distributionFee is correct", () => {
+  for (const s of distributionFeeScenarios) {
+    describe(s.text, () => {
+      const {error} = distributionFeeSchema.validate(s.distributionFee);
+
+      it(`is a${s.expected ? " valid" : "n invalid"} distributionFee`, () => {
+        Boolean(!error).should.eql(Boolean(s.expected));
+      });
+
+      if (!s.expected) {
+        it(`should have the error message: '${s.error}'`, () => {
+          error.details[0].message.should.contain(s.error);
         });
       }
     });
