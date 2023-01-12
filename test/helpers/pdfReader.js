b822828c7540d180ca9ee1bf4c1924931b0fa3eb
MrGuzior <konrad.guzior@gmail.com>
2023-01-12 12:05:50 +0100

    Don't go in to pdfJSON object

--- test/helpers/pdfReader.js
+++ test/helpers/pdfReader.js
@@ -3,7 +3,7 @@
 function parsedPDF(pdfJSON) {
   const dateRegex = /(([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))%20([01]\d|2[0-3])%3A[0-5]\d%3A[0-5]\d)/g;
   let i = 1;
-  return pdfJSON.formImage.Pages.map((page) => {
+  return pdfJSON.Pages.map((page) => {
     let j = 0;
     const pageRows = page.Texts.reduce((rows, text) => {
       const thisRow = text.y < 0 ? "0" : text.y.toString();
