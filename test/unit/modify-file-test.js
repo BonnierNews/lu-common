2d344bace7a9f03c15314b2744e9366c65b63c72
Johan Arkad <johan.arkad@bonniernews.se>
2023-02-22 16:57:15 +0100

    remove extra 'should'

--- test/unit/modify-file-test.js
+++ test/unit/modify-file-test.js
@@ -3,7 +3,7 @@
 const {modifyFile} = require("../helpers/file-utils");
 
 describe("modify file", () => {
-  it("should should modify a file", () => {
+  it("should modify a file", () => {
     const input = '{"some-key":"some-value"}\n{"some-key":"some-other-value"}\n{"some-key":"some-third-value"}\n';
     const expected =
       '{"some-key":"some-value","added-key":"added-value"}\n{"some-key":"some-other-value","added-key":"added-value"}\n{"some-key":"some-third-value","added-key":"added-value"}\n';
