a769ae443e47a85369eb9fc5347fffe983ca41ef
Johan Arkad <johan.arkad@bonniernews.se>
2023-03-29 16:09:15 +0200

    postal delivery only namespaces

---
+++ test/unit/namespaces-postal-delivery-only-test.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const {postalDeliveryOnly} = require("../../lib/namespaces");
+
+const scenarios = [
+  {
+    expected: true,
+    namespace: "paf",
+    text: "Privata Affärer"
+  },
+  {
+    expected: false,
+    namespace: "some-namespace",
+    text: "Some namespace"
+  }
+];
+
+describe("check if postal delivery only", () => {
+  for (const s of scenarios) {
+    describe(s.text, () => {
+      const postOk = postalDeliveryOnly(s.namespace);
+
+      it(`should${!s.expected ? " not" : ""} only allow postal delivery`, () => {
+        Boolean(postOk).should.eql(Boolean(s.expected));
+      });
+    });
+  }
+});
