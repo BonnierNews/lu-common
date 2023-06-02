6687e472d8e6dc20bfebc8f143134df9ceb23c11
Alexi Rahman <alexi.rahman@expressen.se>
2023-06-02 15:46:48 +0200

    Don't use keyFilename for GCS when in GCP

---
+++ test/unit/utils/gcs-test.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const config = require("exp-config");
+const {getCredentials} = require("../../../lib/utils/gcs");
+
+describe("gcs", () => {
+  it("should return whole credentials object for AWS", () => {
+    const credentials = getCredentials();
+    credentials.should.eql(config.gcs.credentials);
+  });
+
+  it("should credentials object without keyFilename for GCP", () => {
+    config.livesIn = "GCP";
+
+    const credentials = getCredentials();
+    const expected = JSON.parse(JSON.stringify(config.gcs.credentials));
+    delete expected.keyFilename;
+    credentials.should.eql(expected);
+
+    delete config.livesIn;
+  });
+});
