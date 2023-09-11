84f2a50aaf810e656ae11d81cf1b66867b95faa4
Simon Varney <varney@users.noreply.github.com>
2023-09-11 15:23:03 +0200

    Add toggle for simplifying PAF migration to common namespace (#88)
    * Add toggle for simplifying PAF migration to common namespace

--- test/unit/namespaces-common-test.js
+++ test/unit/namespaces-common-test.js
@@ -1,8 +1,33 @@
 "use strict";
 
 const { isCommonNamespace, getCommonNamespaces } = require("../../lib/namespaces");
+// FIXME: get rid of expectedNamespacesOld + related test once paf have migrated
+const expectedNamespaces = [ "bnlo", "dn", "expressen", "paf" ];
+const expectedNamespacesOld = [ "bnlo", "dn", "expressen" ];
 
-const expectedNamespaces = [ "bnlo", "dn", "expressen" ];
+describe("isCommonNamespace (paf toggled off)", () => {
+  beforeEach(() => {
+    process.env["NODE-DISABLE-TOGGLE"] = "pafInCommonNamespaces";
+  });
+  after(() => {
+    process.env["NODE-DISABLE-TOGGLE"] = undefined;
+  });
+  expectedNamespacesOld.forEach((namespace) => {
+    it(`should confirm '${namespace}' as part of common namespace`, () => {
+      const result = isCommonNamespace(namespace);
+      result.should.equal(true);
+    });
+  });
+
+  it("should NOT confirm 'di' as part of common namespace", () => {
+    const result = isCommonNamespace("di");
+    result.should.equal(false);
+  });
+
+  it("should give us a list of common namespaces", () => {
+    JSON.stringify(getCommonNamespaces().sort()).should.equal(JSON.stringify(expectedNamespacesOld));
+  });
+});
 
 describe("isCommonNamespace", () => {
   expectedNamespaces.forEach((namespace) => {
