ebbf866a506d3d2b23705355c7b43282aa707f7b
Simon Varney <varney@users.noreply.github.com>
2023-10-02 17:21:26 +0200

    Make index test more generic

--- test/feature/index-test.js
+++ test/feature/index-test.js
@@ -2,6 +2,35 @@
 
 const common = require("../../index");
 const expect = require("chai").expect;
+const fs = require("fs");
+const path = require("path");
+
+const paths = [ "lib", "lib/helpers", "lib/utils", "lib/validation-helpers", "test/helpers" ];
+const allExports = [];
+for (const basePath of paths) {
+  const normalizedPath = path.join(__dirname, "..", "..", basePath);
+  fs.readdirSync(normalizedPath).forEach((file) => {
+    const filePath = path.join(normalizedPath, file);
+    const stats = fs.statSync(filePath);
+    if (stats.isDirectory()) return; // don't traverse subdirectories
+    console.log("file :>> ", file);
+    let importName;
+    switch (file) {
+      case "pdf.js":
+        importName = "PDF";
+        break;
+      case "strip-joi-schema-tags.js":
+        importName = "stripSchemaTag";
+        break;
+      default:
+        importName = toCamelCase(file.replace(".js", ""));
+        break;
+    }
+    console.log("importName :>> ", importName);
+    allExports.push(importName);
+  });
+}
+console.log("allExports :>> ", allExports);
 
 const expectedExports = [
   "caseBodyHelper",
@@ -14,6 +43,7 @@ const expectedExports = [
   "gcpAuth",
   "gcs",
   "http",
+  "swedishBankday",
   "iterators",
   "PDF",
   "pdfGenerator",
@@ -39,17 +69,27 @@ const expectedExports = [
 
 describe("Exposed features", () => {
   const exports = [];
-
   for (const c in common) {
     exports.push(c.toString());
   }
 
-  describe("Importing default export", () => {
-    it("The right stuff gets imported", () => {
-      const list = exports.filter((val) => !expectedExports.includes(val));
-      const list2 = expectedExports.filter((val) => !exports.includes(val));
-      expect(list.length).to.equal(0);
-      expect(list2.length).to.equal(0);
-    });
+  describe("everything exported as lu-common is actually exported", () => {
+    for (const actualExport of exports) {
+      it(`should export ${actualExport}`, () => {
+        allExports.should.include(actualExport);
+      });
+    }
+  });
+
+  describe("everything exported is exposed as lu-common", () => {
+    for (const expectedExport of allExports) {
+      it(`should export ${expectedExport}`, () => {
+        exports.should.include(expectedExport);
+      });
+    }
   });
 });
+
+function toCamelCase(fileName) {
+  return fileName.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+}
