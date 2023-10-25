a52aa12863d7b5faafaf4ecfc68172020d428574
Simon Varney <varney@users.noreply.github.com>
2023-10-25 14:06:06 +0200

    bbm-news will go separately

--- test/unit/namespaces-common-test.js
+++ test/unit/namespaces-common-test.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const { isCommonNamespace, getCommonNamespaces } = require("../../lib/namespaces");
-// FIXME: get rid of expectedNamespacesNoDi, expectedNamespacesNoBbm + related test once di and bbm have migrated
+// FIXME: get rid of expectedNamespacesNoDi, expectedNamespacesNoBbm, expectedNamespacesNoBbmNews + related test once di and bbm have migrated
 const expectedNamespaces = [
   "bbm-aktuellhallbarhet",
   "bbm-byggindustrin",
@@ -35,7 +35,23 @@ const expectedNamespacesNoDi = [
   "expressen",
   "paf",
 ];
-const expectedNamespacesNoBbm = [ "bnlo", "di", "dn", "expressen", "paf" ];
+const expectedNamespacesNoBbm = [ "bbm-news", "bnlo", "di", "dn", "expressen", "paf" ];
+const expectedNamespacesNoBbmNews = [
+  "bbm-aktuellhallbarhet",
+  "bbm-byggindustrin",
+  "bbm-dagensmedicin",
+  "bbm-dagenssamhalle",
+  "bbm-dagligvarunytt",
+  "bbm-dam",
+  "bbm-fastighetsnytt",
+  "bbm-market",
+  "bbm-res",
+  "bnlo",
+  "di",
+  "dn",
+  "expressen",
+  "paf",
+];
 
 describe("isCommonNamespace (di toggled off)", () => {
   beforeEach(() => {
@@ -82,6 +98,30 @@ describe("isCommonNamespace (bbm-* toggled off)", () => {
 
   it("should give us a list of common namespaces", () => {
     JSON.stringify(getCommonNamespaces().sort()).should.equal(JSON.stringify(expectedNamespacesNoBbm));
+  });
+});
+
+describe("isCommonNamespace (bbm-news toggled off)", () => {
+  beforeEach(() => {
+    process.env["NODE-DISABLE-TOGGLE"] = "bbmNewsInCommonNamespaces";
+  });
+  after(() => {
+    process.env["NODE-DISABLE-TOGGLE"] = undefined;
+  });
+  expectedNamespacesNoBbmNews.forEach((namespace) => {
+    it(`should confirm '${namespace}' as part of common namespace`, () => {
+      const result = isCommonNamespace(namespace);
+      result.should.equal(true);
+    });
+  });
+
+  it("should NOT confirm 'bbm-news' as part of common namespace", () => {
+    const result = isCommonNamespace("bbm-news");
+    result.should.equal(false);
+  });
+
+  it("should give us a list of common namespaces", () => {
+    JSON.stringify(getCommonNamespaces().sort()).should.equal(JSON.stringify(expectedNamespacesNoBbmNews));
   });
 });
 
