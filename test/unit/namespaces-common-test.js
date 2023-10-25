4c72555468a5290ad2379b59891195d853d326b2
Simon Varney <varney@users.noreply.github.com>
2023-10-25 13:22:48 +0200

    Add toggle for DI in common namespace (#101)

--- test/unit/namespaces-common-test.js
+++ test/unit/namespaces-common-test.js
@@ -1,7 +1,33 @@
 "use strict";
 
 const { isCommonNamespace, getCommonNamespaces } = require("../../lib/namespaces");
-const expectedNamespaces = [ "bnlo", "dn", "expressen", "paf" ];
+// FIXME: get rid of expectedNamespacesOld + related test once paf have migrated
+const expectedNamespaces = [ "bnlo", "di", "dn", "expressen", "paf" ];
+const expectedNamespacesOld = [ "bnlo", "dn", "expressen", "paf" ];
+
+describe("isCommonNamespace (di toggled off)", () => {
+  beforeEach(() => {
+    process.env["NODE-DISABLE-TOGGLE"] = "diInCommonNamespaces";
+  });
+  after(() => {
+    process.env["NODE-DISABLE-TOGGLE"] = undefined;
+  });
+  expectedNamespacesOld.forEach((namespace) => {
+    it(`should confirm '${namespace}' as part of common namespace`, () => {
+      const result = isCommonNamespace(namespace);
+      result.should.equal(true);
+    });
+  });
+
+  it("should NOT confirm 'bbm-dam' as part of common namespace", () => {
+    const result = isCommonNamespace("bbm-dam");
+    result.should.equal(false);
+  });
+
+  it("should give us a list of common namespaces", () => {
+    JSON.stringify(getCommonNamespaces().sort()).should.equal(JSON.stringify(expectedNamespacesOld));
+  });
+});
 
 describe("isCommonNamespace", () => {
   expectedNamespaces.forEach((namespace) => {
@@ -11,8 +37,8 @@ describe("isCommonNamespace", () => {
     });
   });
 
-  it("should NOT confirm 'di' as part of common namespace", () => {
-    const result = isCommonNamespace("di");
+  it("should NOT confirm 'bbm-dam' as part of common namespace", () => {
+    const result = isCommonNamespace("bbm-dam");
     result.should.equal(false);
   });
 
