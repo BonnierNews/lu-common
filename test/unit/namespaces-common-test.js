fea9e2217ae1cc507d443970a56ab9610b817f7e
Simon Varney <varney@users.noreply.github.com>
2023-10-25 13:58:08 +0200

    Toggle for bbm-* to common namespace

--- test/unit/namespaces-common-test.js
+++ test/unit/namespaces-common-test.js
@@ -1,9 +1,41 @@
 "use strict";
 
 const { isCommonNamespace, getCommonNamespaces } = require("../../lib/namespaces");
-// FIXME: get rid of expectedNamespacesOld + related test once paf have migrated
-const expectedNamespaces = [ "bnlo", "di", "dn", "expressen", "paf" ];
-const expectedNamespacesOld = [ "bnlo", "dn", "expressen", "paf" ];
+// FIXME: get rid of expectedNamespacesNoDi, expectedNamespacesNoBbm + related test once di and bbm have migrated
+const expectedNamespaces = [
+  "bbm-aktuellhallbarhet",
+  "bbm-byggindustrin",
+  "bbm-dagensmedicin",
+  "bbm-dagenssamhalle",
+  "bbm-dagligvarunytt",
+  "bbm-dam",
+  "bbm-fastighetsnytt",
+  "bbm-market",
+  "bbm-news",
+  "bbm-res",
+  "bnlo",
+  "di",
+  "dn",
+  "expressen",
+  "paf",
+];
+const expectedNamespacesNoDi = [
+  "bbm-aktuellhallbarhet",
+  "bbm-byggindustrin",
+  "bbm-dagensmedicin",
+  "bbm-dagenssamhalle",
+  "bbm-dagligvarunytt",
+  "bbm-dam",
+  "bbm-fastighetsnytt",
+  "bbm-market",
+  "bbm-news",
+  "bbm-res",
+  "bnlo",
+  "dn",
+  "expressen",
+  "paf",
+];
+const expectedNamespacesNoBbm = [ "bnlo", "di", "dn", "expressen", "paf" ];
 
 describe("isCommonNamespace (di toggled off)", () => {
   beforeEach(() => {
@@ -12,7 +44,31 @@ describe("isCommonNamespace (di toggled off)", () => {
   after(() => {
     process.env["NODE-DISABLE-TOGGLE"] = undefined;
   });
-  expectedNamespacesOld.forEach((namespace) => {
+  expectedNamespacesNoDi.forEach((namespace) => {
+    it(`should confirm '${namespace}' as part of common namespace`, () => {
+      const result = isCommonNamespace(namespace);
+      result.should.equal(true);
+    });
+  });
+
+  it("should NOT confirm 'di' as part of common namespace", () => {
+    const result = isCommonNamespace("di");
+    result.should.equal(false);
+  });
+
+  it("should give us a list of common namespaces", () => {
+    JSON.stringify(getCommonNamespaces().sort()).should.equal(JSON.stringify(expectedNamespacesNoDi));
+  });
+});
+
+describe("isCommonNamespace (bbm-* toggled off)", () => {
+  beforeEach(() => {
+    process.env["NODE-DISABLE-TOGGLE"] = "bbmInCommonNamespaces";
+  });
+  after(() => {
+    process.env["NODE-DISABLE-TOGGLE"] = undefined;
+  });
+  expectedNamespacesNoBbm.forEach((namespace) => {
     it(`should confirm '${namespace}' as part of common namespace`, () => {
       const result = isCommonNamespace(namespace);
       result.should.equal(true);
@@ -25,7 +81,7 @@ describe("isCommonNamespace (di toggled off)", () => {
   });
 
   it("should give us a list of common namespaces", () => {
-    JSON.stringify(getCommonNamespaces().sort()).should.equal(JSON.stringify(expectedNamespacesOld));
+    JSON.stringify(getCommonNamespaces().sort()).should.equal(JSON.stringify(expectedNamespacesNoBbm));
   });
 });
 
@@ -37,8 +93,8 @@ describe("isCommonNamespace", () => {
     });
   });
 
-  it("should NOT confirm 'bbm-dam' as part of common namespace", () => {
-    const result = isCommonNamespace("bbm-dam");
+  it("should NOT confirm 'whatever' as part of common namespace", () => {
+    const result = isCommonNamespace("whatever");
     result.should.equal(false);
   });
 
