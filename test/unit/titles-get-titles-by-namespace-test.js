c4ef89dc4e6be9da107ec2cd825339a0b92ae747
Simon Varney <varney@users.noreply.github.com>
2024-06-22 08:13:44 +0200

    Functions to return both print and all titles

--- test/unit/titles-get-titles-by-namespace-test.js
+++ test/unit/titles-get-titles-by-namespace-test.js
@@ -1,4 +1,4 @@
-import { getPrintTitlesByNamespace, productConfig } from "../../lib/titles.js";
+import { getTitlesByNamespace, getPrintTitlesByNamespace, productConfig } from "../../lib/titles.js";
 
 const namespaceSafety = [
   { namespace: "dn", text: "DN" },
@@ -9,12 +9,18 @@ const namespaceSafety = [
   { namespace: "gotamedia", text: "Gota Media" },
 ];
 
-describe("getTitlesByNamespace", () => {
+describe("get titles by namespace", () => {
   for (const n of namespaceSafety) {
     describe(n.text, () => {
       it(`should return all titles in namespace: ${n.namespace}`, () => {
-        const titles = getPrintTitlesByNamespace(n.namespace);
+        const titles = getTitlesByNamespace(n.namespace);
         const titlesFromConfig = productConfig[n.namespace].map((pm) => pm.title).filter((t) => t);
+        titles.should.eql(titlesFromConfig);
+      });
+
+      it(`should return all print titles in namespace: ${n.namespace}`, () => {
+        const titles = getPrintTitlesByNamespace(n.namespace);
+        const titlesFromConfig = productConfig[n.namespace].filter((t) => t.tsCode).map((pm) => pm.title).filter((t) => t);
         titles.should.eql(titlesFromConfig);
       });
     });
