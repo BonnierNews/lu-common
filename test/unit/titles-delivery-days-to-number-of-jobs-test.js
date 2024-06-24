e68968f1c02f66750ab67c919d9369fef6676d92
Konrad Guzior <36774785+MrGuzior@users.noreply.github.com>
2024-06-24 14:55:53 +0200

    Add delivery days config functions
    Co-authored-by: Ebba Lindström <ebba.lindstrom@bonniernews.se>

---
+++ test/unit/titles-delivery-days-to-number-of-jobs-test.js
@@ -0,0 +1,122 @@
+import { deliveryDaysToNumberOfJobs } from "../../lib/titles.js";
+
+describe("delivery days to number of jobs", () => {
+  it("should return the correct days", () => {
+    const deliveryDays = [ "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" ];
+    const numberOfJobs = deliveryDaysToNumberOfJobs(deliveryDays);
+    numberOfJobs.should.eql({
+      monday: 1,
+      tuesday: 1,
+      wednesday: 1,
+      thursday: 1,
+      friday: 3,
+      saturday: 0,
+      sunday: 0,
+    });
+  });
+
+  it("should return the correct days", () => {
+    const deliveryDays = [ "tuesday", "thursday", "saturday" ];
+    const numberOfJobs = deliveryDaysToNumberOfJobs(deliveryDays);
+    numberOfJobs.should.eql({
+      monday: 2,
+      tuesday: 0,
+      wednesday: 2,
+      thursday: 0,
+      friday: 3,
+      saturday: 0,
+      sunday: 0,
+    });
+  });
+
+  it("should return the correct days", () => {
+    const deliverDays = [ "friday", "saturday", "sunday" ];
+    const numberOfJobs = deliveryDaysToNumberOfJobs(deliverDays);
+    numberOfJobs.should.eql({
+      monday: 3,
+      tuesday: 0,
+      wednesday: 0,
+      thursday: 1,
+      friday: 3,
+      saturday: 0,
+      sunday: 0,
+    });
+  });
+  it("should return the correct days", () => {
+    const numberOfJobs = deliveryDaysToNumberOfJobs([
+      "monday",
+      "wednesday",
+      "friday",
+      "sunday",
+    ]);
+    numberOfJobs.should.eql({
+      monday: 1,
+      tuesday: 2,
+      wednesday: 0,
+      thursday: 1,
+      friday: 3,
+      saturday: 0,
+      sunday: 0,
+    });
+  });
+  it("should return the correct days", () => {
+    const numberOfJobs = deliveryDaysToNumberOfJobs([
+      "monday",
+      "tuesday",
+      "thursday",
+    ]);
+    numberOfJobs.should.eql({
+      monday: 2,
+      tuesday: 0,
+      wednesday: 2,
+      thursday: 0,
+      friday: 3,
+      saturday: 0,
+      sunday: 0,
+    });
+  });
+
+  it("should return the correct days", () => {
+    const numberOfJobs = deliveryDaysToNumberOfJobs([
+      "monday",
+    ]);
+    numberOfJobs.should.eql({
+      monday: 4,
+      tuesday: 0,
+      wednesday: 0,
+      thursday: 0,
+      friday: 3,
+      saturday: 0,
+      sunday: 0,
+    });
+  });
+
+  it("should return the correct days", () => {
+    const numberOfJobs = deliveryDaysToNumberOfJobs([
+      "monday",
+      "friday",
+    ]);
+    numberOfJobs.should.eql({
+      monday: 3,
+      tuesday: 0,
+      wednesday: 0,
+      thursday: 1,
+      friday: 3,
+      saturday: 0,
+      sunday: 0,
+    });
+  });
+
+  it("should return the correct days", () => {
+    const numberOfJobs = deliveryDaysToNumberOfJobs([ "monday", "tuesday", "thursday", "friday", "saturday", "sunday" ]);
+    numberOfJobs.should.eql({
+      monday: 2,
+      tuesday: 0,
+      wednesday: 1,
+      thursday: 1,
+      friday: 3,
+      saturday: 0,
+      sunday: 0,
+    });
+  });
+});
