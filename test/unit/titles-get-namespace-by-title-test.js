2c174eaaf337613f75cecb054f98995481988a1e
Simon Varney <varney@users.noreply.github.com>
2024-06-24 14:52:43 +0200

    Add get namespace by title function

---
+++ test/unit/titles-get-namespace-by-title-test.js
@@ -0,0 +1,20 @@
+import { getNamespaceByTitle } from "../../lib/titles.js";
+
+const testCases = [
+  { title: "dn", namespace: "dn" },
+  { title: "di", namespace: "di" },
+  { title: "expressen", namespace: "expressen" },
+  { title: "lj", namespace: "bnlo" }, // a local title by real title
+  { title: "ljusnan", namespace: "bnlo" }, // the same local title by alternative title
+  { title: "paf", namespace: "paf" },
+  { title: "smp", namespace: "gotamedia" },
+];
+
+describe("get namespace by title", () => {
+  for (const tc of testCases) {
+    it(`should return ${tc.namespace} for: ${tc.title}`, () => {
+      const namespace = getNamespaceByTitle(tc.title);
+      namespace.should.eql(tc.namespace);
+    });
+  }
+});
