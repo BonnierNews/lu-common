d4209e5872abbc5c58d417491f28d18883e67f1c
Simon Varney <varney@users.noreply.github.com>
2024-07-12 09:17:11 +0200

    Add flag on title to indicate whether we should be expecting a diff file to be created every day or not (#164)
    * Cleanup namespace tests
    
    * Add expectDiffFileEveryDeliveryDay to title config

---
+++ test/unit/titles-get-titles-test.js
@@ -0,0 +1,99 @@
+import {
+  getAllTitles,
+  getAllPrintTitles,
+  getTitleConfig,
+  productMapping,
+  getTitlesByNamespace,
+  getPrintTitlesByNamespace,
+  productConfig,
+} from "../../lib/titles.js";
+
+const namespaceSafety = [
+  { namespace: "dn", text: "DN" },
+  { namespace: "di", text: "DI" },
+  { namespace: "expressen", text: "Expressen" },
+  { namespace: "bnlo", text: "BN Local" },
+  { namespace: "paf", text: "Privata Affärer" },
+  { namespace: "gotamedia", text: "Gota Media" },
+];
+
+describe("get titles", () => {
+  describe("getting all titles", () => {
+    it("should return all titles", () => {
+      const allTitles = getAllTitles();
+      const allTitlesFromConfig = productMapping.filter((pm) => pm.title).map((pm) => pm.title);
+      allTitles.should.eql(allTitlesFromConfig);
+    });
+
+    it("should return all print titles", () => {
+      const allTitles = getAllPrintTitles();
+      const allTitlesFromConfig = productMapping.filter((pm) => pm.tsCode).map((pm) => pm.title);
+      allTitles.should.eql(allTitlesFromConfig);
+    });
+  });
+
+  describe("get titles by namespace", () => {
+    for (const n of namespaceSafety) {
+      describe(n.text, () => {
+        it(`should return all titles in namespace: ${n.namespace}`, () => {
+          const titles = getTitlesByNamespace(n.namespace);
+          const titlesFromConfig = productConfig[n.namespace].map((pm) => pm.title).filter((t) => t);
+          titles.should.eql(titlesFromConfig);
+        });
+
+        it(`should return all print titles in namespace: ${n.namespace}`, () => {
+          const titles = getPrintTitlesByNamespace(n.namespace);
+          const titlesFromConfig = productConfig[n.namespace]
+            .filter((t) => t.tsCode)
+            .map((pm) => pm.title)
+            .filter((t) => t);
+          titles.should.eql(titlesFromConfig);
+        });
+      });
+    }
+  });
+
+  describe("get specific title config", () => {
+    it("should return the config for a title", () => {
+      getTitleConfig("dn").should.eql({
+        namespace: "dn",
+        title: "dn",
+        tsCode: "0440",
+        shortName: "DN",
+        subDirectory: "",
+        type: "paper",
+        productName: "DN",
+        diCustomerSystem: "DNY",
+        diTitle: "Dagens Nyheter",
+        complaintSenderId: "1160",
+        deliveryDays: [ "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" ],
+        expectDiffFileEveryDeliveryDay: true,
+      });
+    });
+    it("should return the config for an alternative title", () => {
+      getTitleConfig("ljusnan").should.eql({
+        namespace: "bnlo",
+        title: "lj",
+        alternativeTitles: [ "ljusnan" ],
+        tsCode: "1230",
+        shortName: "",
+        subDirectory: "",
+        type: "paper",
+        productName: "Ljusnan",
+        diCustomerSystem: "",
+        deliveryDays: [ "monday", "wednesday", "thursday", "friday" ],
+      });
+    });
+    it("should return nothing for an unknown title", () => {
+      should.not.exist(getTitleConfig("whatever"));
+    });
+    [ "di", "dn" ].forEach((title) => {
+      it(`should expect ${title} to have so many subscriptions that a start/stop file is expected every day`, () => {
+        getTitleConfig(title).expectDiffFileEveryDeliveryDay.should.eql(true);
+      });
+    });
+    it("should expect expressen to have so few subscriptions that there won't necessarily be a start/stop file every day", () => {
+      Boolean(getTitleConfig("expressen").expectDiffFileEveryDeliveryDay).should.eql(false);
+    });
+  });
+});
