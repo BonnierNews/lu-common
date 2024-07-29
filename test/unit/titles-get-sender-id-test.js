257606829873c2e2908054a437910982e9b08fe2
Simon Varney <varney@users.noreply.github.com>
2024-07-29 11:37:48 +0200

    npm run format

--- test/unit/titles-get-sender-id-test.js
+++ test/unit/titles-get-sender-id-test.js
@@ -4,21 +4,25 @@ const allPrintProducts = productMapping.filter((p) => p.title && p.tsCode);
 
 describe("get complaints sender id", () => {
   describe("titles with sender id", () => {
-    allPrintProducts.filter((p) => Boolean(p.complaintSenderId)).forEach(({ namespace, title }) => {
-      it(`${namespace}/${title}`, () => {
-        getSenderId(namespace, title).should.eql(productMapping.find((pm) => pm.title === title).complaintSenderId);
+    allPrintProducts
+      .filter((p) => Boolean(p.complaintSenderId))
+      .forEach(({ namespace, title }) => {
+        it(`${namespace}/${title}`, () => {
+          getSenderId(namespace, title).should.eql(productMapping.find((pm) => pm.title === title).complaintSenderId);
+        });
       });
-    });
   });
   describe("titles without sender id", () => {
-    allPrintProducts.filter((p) => !p.complaintSenderId).forEach(({ namespace, title }) => {
-      it(`${namespace}/${title}`, () => {
-        try {
-          getSenderId(namespace, title);
-        } catch (error) {
-          error.message.should.eql("No senderId for report complaint was found. Update product-mapping config");
-        }
+    allPrintProducts
+      .filter((p) => !p.complaintSenderId)
+      .forEach(({ namespace, title }) => {
+        it(`${namespace}/${title}`, () => {
+          try {
+            getSenderId(namespace, title);
+          } catch (error) {
+            error.message.should.eql("No senderId for report complaint was found. Update product-mapping config");
+          }
+        });
       });
-    });
   });
 });
